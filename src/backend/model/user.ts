1212beba22dbf156d50ff79ca0d1a3e0e93ca4d8
Favour chuks <84434963+Favour-chuks@users.noreply.github.com>
2024-10-13 17:19:03 +0100

    Update user.ts

--- src/backend/model/user.ts
+++ src/backend/model/user.ts
@@ -3,8 +3,9 @@ import * as mongoose from 'mongoose'
 // Define the user schema
 const userSchema = new mongoose.Schema({
   _id: {
-    type: String,  // Custom ID as a string (e.g., UUID or custom format)
-    required: true
+    type: String,
+    required: true,
+    unique: true
   },
   username: {
     type: String,
@@ -15,12 +16,25 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
   },
+  localFolderId: {
+    type: String,
+    required: true
+  },
+  driveFolderId: {
+    type: String,
+    required: true
+  },
   role: {
     type: String,
     enum: ['admin', 'user', 'guest'],  // Custom property with specific values
     default: 'user'
   },
   createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  lastUsed: {
     type: Date,
     default: Date.now
   },
@@ -30,8 +44,29 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Create the model from the schema
-const User = mongoose.model('User', userSchema);
+// Middleware to update `lastUsed` on find, findOne, and findOneAndUpdate
+userSchema.pre('find', function(next) {
+  this.update({}, { $set: { lastUsed: Date.now() } });
+  next();
+});
 
+userSchema.pre('findOne', function(next) {
+  this.update({}, { $set: { lastUsed: Date.now() } });
+  next();
+});
+
+userSchema.pre('findOneAndUpdate', function(next) {
+  this.update({}, { $set: { lastUsed: Date.now() } });
+  next();
+});
+
+// Middleware to update `lastUsed` on save
+userSchema.pre('save', function(next) {
+  this.lastUsed = Date.now();
+  next();
+});
+
+// Create a model from the schema
+const User = mongoose.model('User', userSchema);
 
 export default User;
